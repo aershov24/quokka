1d63e1661ac1b73f8ad52db3088ebf7d4214680a
Alex <aershov24@gmail.com>
2016-01-21 10:05:21 +0800

    Image viewer

---
+++ src/public/libs/fancybox.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+      $(".fancybox").fancybox({
+        openEffect  : 'fade',
+        closeEffect : 'fade',
+        nextEffect : 'none',
+        prevEffect : 'none',
+      });
+    });
